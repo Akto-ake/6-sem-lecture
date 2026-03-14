06a250cc33bce23f7b1d2b11ead47845db5445e9
Anya <anya@MacBook-Pro-Anya.local>
2026-03-14 16:53:11 +0300

    add login

--- 05_DiffPatchNet/prog.py
+++ 05_DiffPatchNet/prog.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python3
 import asyncio
+import cowsay
+import shlex
 
 clients = {}
 
+class Client:
+    def __init__(self):
+        self.name = None
+        self.queue = asyncio.Queue() 
+
 async def chat(reader, writer):
+    
     me = "{}:{}".format(*writer.get_extra_info('peername'))
     print(me)
-    clients[me] = asyncio.Queue()
+    clients[me] = Client()
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = asyncio.create_task(clients[me].queue.get())
     while not reader.at_eof():
         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
+                arg = shlex.split(q.result().decode())
+                
+                if (arg[0] == "login") and len(arg) == 2:
+                    # login название_коровы — зарегистрироваться под именем название_коровы
+                    name_cow = arg[1]
+                    if name_cow in clients:
+                        writer.write(f"Error. This name is already taken.\n".encode())
+                        break
+                    clients[me].name = name_cow
+                    writer.write(f"You have registered.\n".encode())
+                    
                 for out in clients.values():
                     if out is not clients[me]:
                         await out.put(f"{me} {q.result().decode().strip()}")
             elif q is receive:
-                receive = asyncio.create_task(clients[me].get())
+                receive = asyncio.create_task(clients[me].queue.get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
     send.cancel()
