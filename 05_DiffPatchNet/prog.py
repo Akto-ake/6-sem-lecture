33837cc46760624793d7498a58de197ecc6df341
Anya <anya@MacBook-Pro-Anya.local>
2026-03-14 18:23:33 +0300

    add 'say' and 'yield'

--- 05_DiffPatchNet/prog.py
+++ 05_DiffPatchNet/prog.py
@@ -11,10 +11,11 @@ class Client:
         self.queue = asyncio.Queue() 
 
 async def chat(reader, writer):
+    me = Client()
     flag_quit = False
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    clients[me] = Client()
+    me_peer = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(me_peer)
+    # clients[me] = Client()
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(clients[me].queue.get())
     while not reader.at_eof():
@@ -29,9 +30,17 @@ async def chat(reader, writer):
                     name_cow = arg[1]
                     if name_cow in clients:
                         writer.write(f"Error. This name is already taken.\n".encode())
+                        await writer.drain()
                         break
+                        
+                    if name_cow not in cowsay.list_cows():
+                        writer.write(f"Error. Unknown cow.\n".encode())
+                        await writer.drain()
+                        break
+                    
                     clients[me].name = name_cow
                     writer.write(f"You have registered.\n".encode())
+                    await writer.drain()
                 
                 elif arg[0] == 'who' and len(arg) == 1:
                     who_list = []
@@ -40,21 +49,46 @@ async def chat(reader, writer):
                         if clients[i].name:
                             who_list.append(clients[i].name)
                     writer.write(f'{"\n".join(who_list)}\n'.encode())
+                    await writer.drain()
                 
                 elif arg[0] == 'cows' and len(arg) == 1:
                     cow_list = [i for i in cowsay.list_cows() if i not in clients.keys()]
                     # cows — просмотр свободных имён коров
                     writer.write(f'{"\n".join(cow_list)}\n'.encode())  
+                    await writer.drain()
                     
                 elif arg[0] == 'quit' and len(arg) == 1:
                     # quit — отключиться
                     flag_quit = True
+                    break
                     
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
+                elif arg[0] == 'say' and len(arg) >= 3:
+                    # say название_коровы текст сообщения — послать сообщение пользователю название_коровы
+                    name_c = arg[1]
+                    message = " ".join(arg[2:])
+
+                    if name_c not in clients:
+                        writer.write("Error. No such user.\n".encode())
+                        await writer.drain()
+                        continue
+
+                    await clients[name_c].queue.put(cowsay.cowsay(message, cow=me.name))
+                    writer.write("Message sent.\n".encode())
+                    await writer.drain()
+                
+                elif arg[0] == 'yield' and len(arg) >= 2:
+                    # yield текст сообщения — послать сообщение всем зарегистрированным пользователям
+    
+                    message = " ".join(arg[1:])
+                    for name, client in clients.items():
+                        if name != me.name:
+                            await client.queue.put(cowsay.cowsay(message, cow=me.name))
+                else:
+                    writer.write("Error. Unknown command.\n".encode())
+                    await writer.drain()
+                    
             elif q is receive:
-                receive = asyncio.create_task(clients[me].queue.get())
+                receive = asyncio.create_task(me.queue.get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
         if flag_quit:
